4df9824bb60658850d68b4c90e6a680d1caaf4fc
luigifurlani13 <luis.furlan@icloud.com>
2021-07-28 22:45:01 +0000

    saving progress

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
+		<p className="footer">
 			Made with <i className="fas fa-music" /> by{" "}
 			<a href="https://www.instagram.com/luigifurlani/">Luis Furlan</a>
 		</p>
